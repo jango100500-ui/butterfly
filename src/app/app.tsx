c9c4a91342529ad7c1217269ca7656bc9bb11227
jango100500-ui <jango100500@gmail.com>
2026-09-21 20:26:28 +0300

    Update app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DropZone } from '../uis/drop';
 import { Head } from '../uis/head';
 import { Empty } from '../uis/empty';
 import { Tabs } from '../uis/tabs';
@@ -6,9 +7,11 @@ import { Tabs } from '../uis/tabs';
 export const App: React.FC = () => {
   return (
     <main style={{ position: 'relative', flex: 1, width: '100%', height: '100%' }}>
-      <Head />
-      <Empty />
-      <Tabs />
+      <DropZone>
+        <Head />
+        <Empty />
+        <Tabs />
+      </DropZone>
     </main>
   );
 };
