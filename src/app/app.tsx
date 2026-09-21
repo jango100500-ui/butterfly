36fa5d3783c4d081420dad097d0c73b6e5e2b29f
jango100500-ui <jango100500@gmail.com>
2026-09-21 17:39:49 +0300

    Update app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Empty } from '../uis/empty';
+import { Tabs } from '../uis/tabs';
 
 export const App: React.FC = () => {
   return (
-    <main style={{ flex: 1, width: '100%', height: '100%' }}>
+    <main style={{ position: 'relative', flex: 1, width: '100%', height: '100%' }}>
       <Empty />
+      <Tabs />
     </main>
   );
 };
